e4ae9b3dd1af0f87244e9c5db2e525f4e2453a87
DavidPereira2007 <david@outlook.com.br>
2026-06-05 14:18:07 -0300

    Update Structure

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@
 using Exercice;
 class Program
 {
-    static void Main()
+    private static void averageExercice()
     {
-        Console.WriteLine("Run program");
         double[] grades = { 6.5, 5.0, 6.0, 5.2 }; // grades exemple for testing
         double average = GradeAverage.Calculateaverage(grades);
         Console.WriteLine($"The average grade is: {average:F2}");
     }
+    
+    static void Main()
+    {
+        Console.WriteLine("Run program");
+        averageExercice();
+    }
 }
+        
